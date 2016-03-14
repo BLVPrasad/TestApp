ab6468b54b609b65430e81d9ed8424f030ffec71
blvprasad <lbokka@DIN76000161.corp.capgemini.com>
2016-03-14 12:43:44 +0530

    ok

---
+++ MavenTest/src/main/java/com/prasad/MavenTest/App.java
@@ -0,0 +1,15 @@
+package com.prasad.MavenTest;
+
+import java.util.UUID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
